39b5eb900e63446f8e253e408437a9b8828cd83b
Shaked178 <aesgbcr@gmail.com>
2026-01-06 21:28:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import pandas as pd
+import joblib
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
 
+data = pd.read_csv('parkinsons.csv')
+
+X = data.drop(['status', 'name'], axis=1)
+y = data['status']
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+model = RandomForestClassifier(random_state=42)
+model.fit(X_scaled, y)
+
+joblib.dump(model, 'model.joblib')
+joblib.dump(scaler, 'scaler.joblib')
+
+print("Done")
