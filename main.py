47e6880ad67f784d7b28cc5c46eb7a6968327ec3
Shaked178 <aesgbcr@gmail.com>
2026-01-06 22:12:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,20 @@ from sklearn.neighbors import KNeighborsClassifier
 
 data = pd.read_csv('parkinsons.csv')
 
-X = data[['PPE', 'spread1']]
+X = data[['PPE', 'spread1', 'MDVP:Fo(Hz)']]
 y = data['status']
 
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=100)
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
 
 scaler = MinMaxScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_val_scaled = scaler.transform(X_val)
 
 model = KNeighborsClassifier(n_neighbors=3)
-model.fit(X_train_scaled, y_train) 
+model.fit(X_train_scaled, y_train)
 
-print(model.score(X_val_scaled, y_val))
+accuracy = model.score(X_val_scaled, y_val)
+print(accuracy)
 
 joblib.dump(model, 'model.joblib')
 joblib.dump(scaler, 'scaler.joblib')
