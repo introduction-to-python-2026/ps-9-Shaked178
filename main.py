45dedab3d88dd46ca3a875f90245e7eaa614c136
Shaked178 <aesgbcr@gmail.com>
2026-01-06 22:13:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ data = pd.read_csv('parkinsons.csv')
 X = data[['PPE', 'spread1', 'MDVP:Fo(Hz)']]
 y = data['status']
 
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=100)
 
 scaler = MinMaxScaler()
 X_train_scaled = scaler.fit_transform(X_train)
